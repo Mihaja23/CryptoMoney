5097cae5bc052319c7ec58d57c5fb14e262d49b5
Mihaja <mihaja.randriamaromanana@efrei.net>
2022-10-19 14:37:51 +0200

    Modification

--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "./components/Home.vue";
 import Login from "./components/Login.vue";
 import Register from "./components/Register.vue";
-// lazy-loaded
+
 const Profile = () => import("./components/Profile.vue")
 const BoardAdmin = () => import("./components/BoardAdmin.vue")
 const BoardModerator = () => import("./components/BoardModerator.vue")
